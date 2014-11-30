097e22222474252d5d5d715d32b15c476ecc1bce
Rory <=>
2014-11-30 14:50:47 +0000

    morgan instead of logger

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var gzippo = require('gzippo');
 var express = require('express');
 var modRewrite = require('connect-modrewrite');
+var morgan = require('morgan');
 
 var app = express();
  
-app.use(express.logger('dev'));
+app.use(morgan('combined'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.listen(process.env.PORT || 5000);
