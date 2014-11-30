66d2522e42c509d86fa3148b923d2790e4ef3f62
Rory <=>
2014-11-30 15:20:44 +0000

    static serving of assets

--- web.js
+++ web.js
@@ -1,8 +1,14 @@
 var gzippo = require('gzippo');
 var express = require('express');
 var modRewrite = require('connect-modrewrite');
+var serveStatic = require('serve-static');
 
 var app = express();
  
+app.use("/scripts", express.static(__dirname + "/app/scripts"));
+app.use("/fonts", express.static(__dirname + "/app/fonts"));
+app.use("/images", express.static(__dirname + "/app/images"));
+app.use("/styles", express.static(__dirname + "/app/styles"));
+app.use("/views", express.static(__dirname + "/app/scripts"));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.listen(process.env.PORT || 5000);
