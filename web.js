e15662d198a2b83f280e5e68b6c7fc16299351ef
Rory <=>
2014-11-30 15:34:54 +0000

    gzippo routes updated

--- web.js
+++ web.js
@@ -1,13 +1,16 @@
 var gzippo = require('gzippo');
 var express = require('express');
 var modRewrite = require('connect-modrewrite');
+var routes = require('./routes');
+var http = require('http');
+var path = require('path');
 
 var app = express();
  
-app.use("/scripts", express.static(__dirname + "/app/scripts"));
-app.use("/fonts", express.static(__dirname + "/app/fonts"));
-app.use("/images", express.static(__dirname + "/app/images"));
-app.use("/styles", express.static(__dirname + "/app/styles"));
-app.use("/views", express.static(__dirname + "/app/scripts"));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+app.use("/scripts", gzippo.staticGzip(__dirname + "/dist/app/scripts"));
+app.use("/fonts", gzippo.staticGzip(__dirname + "/dist/app/fonts"));
+app.use("/images", gzippo.staticGzip(__dirname + "/dist/app/images"));
+app.use("/styles", gzippo.staticGzip(__dirname + "/dist/app/styles"));
+app.use("/views", gzippo.staticGzip(__dirname + "/dist/app/scripts"));
 app.listen(process.env.PORT || 5000);
