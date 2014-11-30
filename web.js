742751e1c9d4abc0ed1d63c98433a1b206a64a40
Rory <=>
2014-11-30 14:52:43 +0000

    remove morgan

--- web.js
+++ web.js
@@ -1,10 +1,8 @@
 var gzippo = require('gzippo');
 var express = require('express');
 var modRewrite = require('connect-modrewrite');
-var morgan = require('morgan');
 
 var app = express();
  
-app.use(morgan('combined'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.listen(process.env.PORT || 5000);
