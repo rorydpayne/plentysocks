e3b2ba91d663a9f3933b644ff37a28734d564b01
Rory <=>
2014-11-30 14:37:03 +0000

    middleware added

--- web.js
+++ web.js
@@ -1,5 +1,22 @@
 var gzippo = require('gzippo');
 var express = require('express');
+var bodyParser = require('body-parser');
+var compression = require('compression');
+var timeout = require('connect-timeout');
+var cookieParser = require('cookie-parser');
+var session = require('cookie-session');
+var methodOverride = require('method-override');
+var csrf = require('csurf');
+var errorhandler = require('errorhandler');
+var session = require('express-session');
+var morgan = require('morgan');
+var responseTime = require('response-time');
+var favicon = require('serve-favicon');
+var serveIndex = require('serve-index');
+var serveStatic = require('serve-static');
+var vhost = require('vhost');
+var modRewrite = require('connect-modrewrite');
+
 var app = express();
  
 app.use(express.logger('dev'));
