f182ae3beceaee473d68b7bdcc38ffa5d6c4045a
Rory <=>
2014-11-30 16:35:45 +0000

    connect mod-rewrite in web.js

--- web.js
+++ web.js
@@ -3,13 +3,7 @@ var express = require('express');
 var modRewrite = require('connect-modrewrite');
 
 var app = express();
- 
+
+app.use(modRewrite(['^[^\\.]*$ /index.html [L]'])); 
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-// app.use("/scripts", gzippo.staticGzip(__dirname + "/dist/scripts"));
-// app.use("/fonts/raleway", gzippo.staticGzip(__dirname + "/dist/fonts/raleway"));
-// app.use("/images", gzippo.staticGzip(__dirname + "/dist/images"));
-// app.use("/styles", gzippo.staticGzip(__dirname + "/dist/styles"));
-// app.use("/views", gzippo.staticGzip(__dirname + "/dist/views"));
-// app.use("/views/global", gzippo.staticGzip(__dirname + "/dist/views/global/"));
-// app.use("/views/global/desktop", gzippo.staticGzip(__dirname + "/dist/views/global/desktop"));
 app.listen(process.env.PORT || 5000);
